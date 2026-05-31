ad590edf801aa6e0bf1960fc3f3793ed5a4e4310
Samad <craigbrew269@gmail.com>
2026-05-31 14:55:01 +0100

    fix peer deps for react-paystack

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,18 +3,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAgdpL0y_drLeILH50YMtrcOThdLwbfDAk",
-  authDomain: "shopreact-7db24.firebaseapp.com",
-  projectId: "shopreact-7db24",
-  storageBucket: "shopreact-7db24.firebasestorage.app",
-  messagingSenderId: "146578688896",
-  appId: "1:146578688896:web:821f5654ae401f8532cf65"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Export services to use across the app
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export default app;
